9213034cd23333fecc342bb77ed524bd2e6879e6
francop-dev <francoperuil.dev@gmail.com>
2024-05-20 13:34:42 -0300

    fundamentos flask

---
+++ app/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    jugadores = ['mbappe','jouse','jaimito','roberto carlos']
+    data = {
+        'jugadores':jugadores,
+        'titulo': 'mejores',
+        'bienvenida': '4',
+        'numero_de_jugadores':len(jugadores)
+    }
+    return render_template('index.html', data=data)
+
+if __name__ =='__main__':
+    app.run(debug=True,port=5000)
